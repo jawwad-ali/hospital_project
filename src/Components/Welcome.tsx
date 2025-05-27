6f30d573260e919dc24bb88ab16ce62bf4d108fc
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-05-28 01:31:31 +0500

    fixed image path

--- src/Components/Welcome.tsx
+++ src/Components/Welcome.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
-import BestCare from "../../public/best_Care.png";
+import BestCare from "../../public/best_care.png";
+// import BestCare from "../../public/best_Care.png";
 
 export default function HeroSection() {
   const features = [
